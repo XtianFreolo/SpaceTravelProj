56d74171e471eb0c8c93fc158a07644de030452a
XtianFreolo <F60@umbc.edu>
2025-07-28 22:53:03 -0400

    Updated HomePage.jsx and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import styles from "./App.module.css";
+import HomePage from "./pages/HomePage.jsx";
 
-function App ()
-{
+function App() {
   return (
     <>
       <h1>Space Travel</h1>
+      <HomePage />
     </>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,42 @@
 
 
 function HomePage() {
-    return <div> TODO: HomePage </div>;
+    return (
+        <div>
+            <h1>Welcome to Xtian's Space station mate</h1>
+            <p>
+                Think of this as your Space 7-11
+                Explore the universe with us!
+                Here we have everything you need for your
+                intergalactic journey. You can rent Spaceships
+                and buy supplies for your trip.
+                You can also check out the planets.
+                We have a wide range of planets to explore,
+                all in the Planet section of the app.
+            </p>
+            <br></br>
+
+            <p>
+                <strong>Note:</strong>
+                If you want to rent a spaceship, Please go
+                to the Spaceship Hangar and fill out a form
+                to request a spaceship.
+                Once you submit the form, our team will review
+            </p>
+            <br></br>
+
+            <p>
+                <strong>Note:</strong>
+                To explore a planet, click the planet
+                you want to explore at our planet section and
+                when the planet border goes green, thats a sign
+                for your spaceship to land, click your spaceship
+                in the page and it will land on the planet.
+            </p>
+
+
+        </div>
+    );
 }
 
 
